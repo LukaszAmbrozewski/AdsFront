9bb330d86b0bb14c3c9a1db1b1c48fbfce8e0e90
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-05-22 16:01:22 +0200

    Added context.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import '../../utils/fix-map-icon';  //Pobieramy ikony markerów
+import {SearchContext} from "../../contexts/search.context";
 
+import 'leaflet/dist/leaflet.css';
 import './Map.css';
 
 export const Map = () => {
+    const {search} = useContext(SearchContext);
+
+    //Wysyła zapytanie gdy zmieni sie search
+    useEffect(() => {
+        console.log('Make request to search for', search);
+    }, [search]);
+
     return (
-        <div className="map"/>
+        <div className="map">
+            <h1>Search for: {search}</h1>
+            <MapContainer center={[50.2657152,18.99450008]} zoom={20}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> & contributors"
+                />
+                <Marker position={[50.2657152,18.99450008]}>
+                    <Popup>
+                        <h2>IT.focus</h2>
+                        <p>Super firma programistyczna!</p>
+                    </Popup>
+                </Marker>
+                <Marker position={[50,19]}>
+                    <Popup>
+                        <h2>Nie IT.focus</h2>
+                        <p>Inna firma programistyczna!</p>
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
     );
 }
